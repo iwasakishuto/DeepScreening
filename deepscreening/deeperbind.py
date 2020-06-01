77b5847311bec3faa7e9b5cb9845e22ca396f75e
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-06-02 03:59:29 +0900

    Implement deeperbind (In the future, inherit the Model class of keras and overwrite `fit` method.)

--- deepscreening/deeperbind.py
+++ deepscreening/deeperbind.py
@@ -3,10 +3,10 @@ import numpy as np
 from keras.layers import Input, LSTM, Convolution1D, BatchNormalization
 from keras.models import Model
 
-def deeperbind(params={}):
+def deeperbind(params={}, x_train_data={}, y_train_data={}):
     max_seq_len   = params.get("max_seq_len", 1000)
     num_base_type = params.get("num_base_type", 4)
-    x_in = Input(shape=(None, num_base_type))
+    x_in = Input(shape=(None, num_base_type), name="input_sequence")
 
     # Convolutional
     num_conv_layers   = params.get("num_conv_layers", 6)
@@ -30,13 +30,14 @@ def deeperbind(params={}):
 
     # stacked LSTM layers
     num_lstm_layers     = params.get("num_lstm_layers", 2)
-    latent_space_dim    = params.get("latent space_dim", 128)
+    latent_space_dim    = params.get("latent_space_dim", 128)
     latent_space_dim_gf = params.get("latent_space_dim_gf", 1.4928245388)
     lstm_activation     = params.get("lstm_activation", "tanh")
 
     return_sequences = True
     for j in range(num_lstm_layers):
-        if j==num_lstm_layers-1: return_sequences = False
+        if j==num_lstm_layers-1:
+            return_sequences = False
         x = LSTM(units=int(latent_space_dim * latent_space_dim_gf**(num_lstm_layers-j-1)),
                  return_sequences=return_sequences,
                  activation=lstm_activation,
@@ -48,7 +49,7 @@ def deeperbind(params={}):
     is_batchnorm_dense = params.get("is_batchnorm_dense", True)
     dense_dropout_rate = params.get("dense_dropout_rate", 0.25)
 
-    for j in range(num_dense_layers):
+    for j in range(num_dense_layers-1):
         x = Dense(units=latent_space_dim,
                   activation=dense_activation,
                   name=f"deeperbind_dense{j}")(x)
@@ -56,5 +57,6 @@ def deeperbind(params={}):
             x = Dropout(rate=dense_dropout_rate**(j+1))(x)
         if is_batchnorm_dense:
             x = BatchNormalization(axis=-1, name=f"deeperbind_dense_norm{j}")(x)
+    x = Dense(units=latent_space_dim, activation="linear", name="embedding")(x)
 
     return Model(x_in, x)
